504d1b936cba19fabfdc1d831f1bd09625a0ba69
Krešimir Forjan <kreso2806@gmail.com>
2021-03-19 02:42:48 +0100

    Added complete project

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace rssv_lv1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(238, 193);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(103, 60);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(238, 124);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(103, 63);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(225, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Unesite kriticnu vrijedonst";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 71);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ucitaj datoteku";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 474);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace rssv_lv1
+{
+    public partial class Form1 : Form
+    {
+        private System.Timers.Timer t;
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Enabled = false;
+            t = new System.Timers.Timer(1000);
+            t.Elapsed += new System.Timers.ElapsedEventHandler(vrijeme);
+
+        }
+        
+
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            checkInputValue(richTextBox2.Text);
+        }
+
+        public void checkInputValue(string value)
+        {
+            if (!int.TryParse(value, out int i))
+            {
+                button1.Enabled = false;
+                label1.Text = "Niste unijeli broj ili je prazno polje";
+                return;
+            }
+            label1.Text = "Kritična vrijednost";
+            button1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (t.Enabled == false)
+            {
+                //Pokretanje timer-a.
+                t.Start(); //Odgovara: t.Enabled = true;
+                button1.Text = "Zaustavi";
+            }
+            else
+            {
+                //Zaustavljanje timer-a
+                t.Stop(); //Odgovara: t.Enabled = false;
+                button1.Text = "Pokreni";
+            }
+        }
+
+        
+        private void vrijeme(object s, EventArgs e)
+        {
+                StreamReader citac = new StreamReader("value.txt");
+                int criticalValue = Int32.Parse(richTextBox2.Text);
+                int[] valueArray =
+                Array.ConvertAll(citac.ReadLine().Split(',').ToArray(), int.Parse);
+                citac.Close();
+
+
+            Invoke((MethodInvoker)delegate //Anonimna metoda
+            {
+                if (i >= valueArray.Length)
+                {
+                    t.Stop();
+                    MessageBox.Show("Provjera gotova, nema vrijednosti vecih od kriticne.");
+                    i = 0;
+                    richTextBox1.Text = "";
+                    button1.Text = "Pokreni";
+                    return;
+
+                }
+                richTextBox1.Text = valueArray[i].ToString();
+                Application.DoEvents();
+                if (valueArray[i] > criticalValue)
+                {
+                    t.Stop();
+                    MessageBox.Show(valueArray[i].ToString() + " je veci od kriticne vrijednosti, prekidam program.");
+                    i = 0;
+                    richTextBox1.Text = "" ;
+                    button1.Text = "Pokreni";
+                    return;
+                }
+                i++;
+            });
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
